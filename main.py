a155c3f1c71a2299e229ff32dae33f2589c8fdee
mecr4x <serzhik_adamyan2011@mail.ru>
2026-03-06 18:57:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
+from aiohttp import web
 import json
 import time
 
