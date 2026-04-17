368dff14d0eb10547c6a557a25e3b4db15c8c29d
mecr4x <serzhik_adamyan2011@mail.ru>
2026-04-18 00:19:28 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,30 @@
 import sqlite3
-import json
-from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+
+DATABASE_URL = 'sqlite:///admin.db'
+
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(String, index=True, unique=True, nullable=False)
+    name = Column(String, nullable=True)
+    registered_at = Column(DateTime, default=datetime.datetime.now)
+    active = Column(Boolean, default=True)
+
+class Broadcast(Base):
+    __tablename__ = 'broadcasts'
+    id = Column(Integer, primary_key=True, index=True)
+    message = Column(String, nullable=False)
+    send_at = Column(DateTime, default=datetime.datetime.now)
+
+Base.metadata.create_all(bind=engine)
 
 
 class Database:
